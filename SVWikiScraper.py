38e4225e0d048390eb557995a056b532e701d982
edmanf <edmanfeng@hotmail.com>
2020-01-06 17:01:04 -0800

    refactor

--- SVWikiScraper.py
+++ SVWikiScraper.py
@@ -3,8 +3,8 @@ from urllib.request import urlopen
 from urllib.parse import urljoin
 import sqlite3
 
-global SCRAPE_COUNTER, SCRAPE_LIMIT, PROGRESS
-PROGRESS = False # Enable the printing of messages to track progress
+global SCRAPE_COUNTER, SCRAPE_LIMIT, LOG_PROGRESS
+LOG_PROGRESS = True # Enable the printing of messages to track progress
 SCRAPE_COUNTER = 0
 SCRAPE_LIMIT = 10000 # Limit the number page opens for bandwidth
 
@@ -19,7 +19,7 @@ def scrape_villager_names():
     names = [gt.text.strip('\n') for gt in soup.find_all(class_="gallerytext")]
     return names
     
-def write_village_names_to_file(names, file_path):
+def write_villager_names_to_file(names, file_path):
     """
         Writes the list of names to a file specified by file_path. Will create
         a file if does not exist. Names are seperated by newlines.
@@ -33,48 +33,13 @@ def scrape_items_to_db():
         Returns a list of items
     """
     base = "https://stardewvalleywiki.com/"
-    path = "/Category:Items"
+    path = "Category:Items"
     
     item_links = get_item_links(urljoin(base, path))
-    write_giftable_items(item_links)
+    gift_reactions = get_giftable_item_reactions_from_links(item_links)
+    write_gift_reactions_to_db(gift_reactions)
     
     
-def write_giftable_items(item_links):
-    if PROGRESS:
-        print("Writing " + str(len(item_links)) + " items to db...")
-        
-    conn = sqlite3.connect("SDV.db")
-    c = conn.cursor()
-    for link in item_links:
-        if PROGRESS:
-            print("Opening---" + link)
-        soup = bs4.BeautifulSoup(urlopen(link).read(), features="html.parser")
-        item = soup.find(id="firstHeading").text.strip("\n ")
-        if PROGRESS:
-            print("Writing " + item + "...")
-        gift_header = soup.find(id="Gifting")
-        if not gift_header:
-            # Item is not giftable
-            continue
-
-        rows = gift_header.parent.find_next("table").find_all("tr")
-        
-        table_name = rows[0].text.strip("\n ")
-        if table_name != "Villager Reactions":
-            continue
-        
-        
-        c.execute("INSERT INTO items VALUES (?)", (item,))
-        for row in rows[1:]:
-            reaction = row.find("th").text.strip("\n ")
-            villagers = [x.text.strip("\n ") for x in row.find_all("div")]
-            for villager in villagers:
-                c.execute("INSERT INTO gifts VALUES (?, ?, ?)",
-                               (villager, item, reaction))
-    conn.commit()
-    conn.close()
-            
-        
 def get_item_links(url):
     """ Returns a set of item urls from this url and its sub-categories
     
@@ -92,7 +57,7 @@ def get_item_links(url):
     if SCRAPE_COUNTER >= SCRAPE_LIMIT:
         print("Limit reached! " + url + " skipped!")
         return []
-    if PROGRESS:
+    if LOG_PROGRESS:
         print("Opening----" + url)
     page = urlopen(url)
     
@@ -109,9 +74,65 @@ def get_item_links(url):
                 end_links = end_links.union(get_item_links(urljoin(url, path)))
             else:
                 end_links.add(urljoin(url, path))
-    if PROGRESS:
+                
+    if LOG_PROGRESS:
         print(url + " scraped!")
     return end_links
+    
+    
+def get_giftable_item_reactions(item_links):
+    """
+        Return a list of GiftReactions scraped from the given list of
+        urls item_links.
+    """
+    if LOG_PROGRESS:
+        print("Writing " + str(len(item_links)) + " items to db...")
+        
+    reactions = []
+    for link in item_links:
+        if LOG_PROGRESS:
+            print("Opening---" + link)
+        soup = bs4.BeautifulSoup(urlopen(link).read(), features="html.parser")
+        
+        item = soup.find(id="firstHeading").text.strip("\n ")
+        
+        gift_header = soup.find(id="Gifting")
+        if not gift_header:
+            # Item is not giftable
+            continue
+
+        rows = gift_header.parent.find_next("table").find_all("tr")
+        table_name = rows[0].text.strip("\n ")
+        if table_name != "Villager Reactions":
+            continue
+        
+        for row in rows[1:]:
+            reaction = row.find("th").text.strip("\n ")
+            villagers = [x.text.strip("\n ") for x in row.find_all("div")]
+            for villager in villagers:
+                reactions.append(GiftReaction(villager, item, reaction))
+                
+    return gift_reactions
+                
+    def write_gift_reactions_to_db(gift_reactions):
+        conn = sqlite3.connect("SDV.db")
+        c = conn.cursor()
+        for gift_reaction in gift_reactions:
+            villager = gift_reaction.villager
+            item = gift_reaction.item
+            reaction = gift_reaction.reaction
+            c.execute("INSERT INTO gifts VALUES (?, ?, ?)",
+                                       (villager, item, reaction))
+        conn.commit()
+        conn.close()
+        
+
+    
+class GiftReaction:
+    def __init__(self, villager, item, reaction):
+        self.villager = villager
+        self.item = item
+        self.reaction = reaction
 
     
 if __name__ == "__main__":
