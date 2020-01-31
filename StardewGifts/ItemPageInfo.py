418b1a711b816b49fc80c538952da1c2c1ab3902
edmanf <edmanfeng@hotmail.com>
2020-01-30 16:40:12 -0800

    Initial commit

---
+++ StardewGifts/ItemPageInfo.py
@@ -0,0 +1,6 @@
+class ItemPageInfo:
+    def __init__(self, name, source, season, gift_reactions):
+        self.name = name
+        self.source = source
+        self.season = season
+        self.gift_reactions = gift_reactions
