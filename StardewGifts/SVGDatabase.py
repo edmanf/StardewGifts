a1ea7654ef5d96256e904c96bce42f8696702e8c
edmanf <edmanfeng@hotmail.com>
2020-02-06 16:53:27 -0800

    fixed db issues with writing lists and added item_attributes

--- StardewGifts/SVGDatabase.py
+++ StardewGifts/SVGDatabase.py
@@ -8,15 +8,17 @@ class SVGDatabase:
     
     def __init__(self, 
         database_path, 
-        gift_table_name = self.DEFAULT_GIFT_TABLE_NAME,
-        item_attributes_table_name = self.DEFAULT_ITEM_ATTRIBUTES_TABLE_NAME):
+        gift_table_name = DEFAULT_GIFT_TABLE_NAME,
+        item_attributes_table_name = DEFAULT_ITEM_ATTRIBUTES_TABLE_NAME):
         
         self.gift_table_name = gift_table_name
-        self.item_attributes_table_name = item__attributes_table_name
+        self.item_attributes_table_name = item_attributes_table_name
         
         self.database_path = database_path
+        self.conn = None
         self.cursor = self.get_cursor()
-        self.build_all()
+        
+        self.build_all(self.cursor)
     
     def write_reactions(self, reactions):
         statement = "INSERT INTO {} VALUES(?, ?, ?)" \
@@ -25,20 +27,22 @@ class SVGDatabase:
         self.write_list(statement, args)
         
     def write_items(self, items):
-        statement = "INSERT INTO {} VALUES(?, ?, ?)" \ 
+        statement = "INSERT INTO {} VALUES(?, ?, ?)" \
             .format(self.gift_table_name)
         args = [(x.name, x.source, x.season) for x in items]
         self.write_list(statement, args)
         
     def write_item_attributes(self, items):
-        statement = "INSERT INTO {} VALUES(?, ?, ?)" \ 
-            .format(self.item__attributes_table_name)
+        statement = "INSERT INTO {} VALUES(?, ?, ?)" \
+            .format(self.item_attributes_table_name)
             
         args = []
         for item in items:
             for attribute in item.attributes:
                 for value in item.attributes[attribute]:
-                    args.append(item.name, attribute, value) 
+                    arg = (item.name, attribute, value)
+                    print(arg)
+                    args.append(arg)
         self.write_list(statement, args)
                 
         
@@ -47,11 +51,11 @@ class SVGDatabase:
         the args list. """
         
         for arg in args:
-            self.cursor.execute(statement, args)
+            self.cursor.execute(statement, arg)
         
     def get_cursor(self):
-        self.conn = sqlite3.connect(f"{filename}.db")
-        return conn.cursor()
+        self.conn = sqlite3.connect(f"{self.database_path}.db")
+        return self.conn.cursor()
         
     def commit(self):
         self.conn.commit()
@@ -61,25 +65,25 @@ class SVGDatabase:
         cursor.execute("""CREATE TABLE if not exists {}(
                         name TEXT PRIMARY KEY NOT NULL, 
                         source TEXT NOT NULL,
-                        season TEXT NOT NULL)""" \ 
+                        season TEXT NOT NULL)""" \
                         .format(self.item_table_name))
                       
-    def build_gifts_db(cursor):
+    def build_gifts_db(self,cursor):
         cursor.execute("""CREATE TABLE if not exists {}(
                             villager TEXT NOT NULL, 
                             item TEXT NOT NULL, 
                             reaction TEXT NOT NULL,
-                            PRIMARY KEY(villager, item))""" \ 
+                            PRIMARY KEY(villager, item))""" \
                             .format(self.gift_table_name))
                             
-    def build_item_attributes_db(cursor):
+    def build_item_attributes_db(self, cursor):
         cursor.execute("""CREATE TABLE if not exists {}(
                             item TEXT NOT NULL, 
                             attribute TEXT NOT NULL,
-                            value TEXT NOT NULL
+                            value TEXT NOT NULL,
                             PRIMARY KEY(item, attribute, value))""" \
                             .format(self.item_attributes_table_name))
         
-    def build_all():
-        build_item_attributes_db()
-        build_gifts_db()
+    def build_all(self, cursor):
+        self.build_item_attributes_db(cursor)
+        self.build_gifts_db(cursor)
